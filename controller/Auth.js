9fa4adf7f13d09f32d7673b4a6d0b4e00be7ed22
Tushar <kmrtushar01@gmail.com>
2024-07-29 14:51:43 +0530

    login controller with jwt and cookies

--- controller/Auth.js
+++ controller/Auth.js
@@ -55,3 +55,79 @@ exports.signup = async (req, res) => {
     }
 
 }
+
+
+// Creating login business logic
+exports.login = async (req, res) => {
+    try {
+        // Parsing data from request's body
+        const { email, password } = req.body;
+
+        // if user do not fill all the details
+        if (!email || !password) {
+            return res.json({
+                success: false,
+                message: "Please fill all the details carefully"
+            });
+        }
+        // Check user's entry in the database
+        const user = await Authentication.findOne({ email });
+
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User is not registered"
+            });
+        }
+
+        // payload for jwt 
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role,
+        }
+        // Compare the request's body password or actual password , if equals then create the jwt 
+        if (await bcrypt.compare(password, user.password)) {
+            let token = jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h",
+                }
+            );
+
+            // pushing the token to the userObject
+            const userObject = { ...user.toObject() };
+            userObject.token = token;
+
+            // Hide the password from userObject to maintain privacy
+            userObject.password = undefined;
+
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true
+            }
+
+            // Creating Cookie
+            res.cookie("trush", token, options).status(200).json({
+                success: true,
+                token,
+                userObject,
+                message: "User logged in successfully"
+            })
+        }
+        else {
+            return res.status(409).json({
+                success: false,
+                message: "Password Incorrect!"
+            })
+        }
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            success: false,
+            message: "Login failed"
+        });
+    }
+}
